6a9dd59dc928ce95f60840c5c4587fcabb74af2b
jeromecoffin <jerome.coffin@efrei.net>
2020-04-11 15:55:40 +0200

    avancements Camille

--- bot.js
+++ bot.js
@@ -1,32 +1,33 @@
 // Load the SDK
 let RainbowSDK = require('rainbow-node-sdk');
 
+
 // Define your configuration
 let options = {
     "rainbow": {
-        "host": "sandbox",
+        "host": "sandbox",                      
     },
     "credentials": {
-        "login": "rainbow.chatbot.coffinjerome@gmail.com",
-        "password": "*34)\"GNwOy?2"
+        "login": "rainbow.chatbot.camilleleou@gmail.com",  
+        "password": "ir6AWPPiJj^5"   
     },
     // Application identifier
     "application": {
-        "appID": "977a1ef07b0411ea88b46f998ddae648",
-        "appSecret": "u0ObJJemgJk4pdV2JAkx3N4zAYsi8gIzSwgVkPzg3QClaDDHZnspYnKJPt0MEzrg",
+        "appID": "675ac1907b0711ea88b46f998ddae648", 
+        "appSecret": "7PCkrHyo0SgPeKfOrq8tKBI8EDLTUFcjKWQ7skR4ckGxj7u3paYFMr1tqoGE0rpa", 
     },
     // Logs options
     "logs": {
-        "enableConsoleLogs": true,
-        "enableFileLogs": false,
+        "enableConsoleLogs": true,              
+        "enableFileLogs": false,                
         "file": {
             "path": '/var/tmp/rainbowsdk/',
-            "level": 'debug'
+            "level": 'debug'                    
         }
     },
     // IM options
     "im": {
-        "sendReadReceipt": false
+        "sendReadReceipt": false   
     }
 };
 
@@ -34,25 +35,25 @@ let options = {
 let rainbowSDK = new RainbowSDK(options);
 
 // Start the SDK
-rainbowSDK.start();
+rainbowSDK.start().then(() => {
+    // Do something when the SDK is connected to Rainbow
 
-rainbowSDK.events.on("rainbow_onmessagereceived", (message) => {
-    // Check if the message is not from you
-    if(!message.fromJid.includes(rainbowSDK.connectedUser.jid_im)) {
-        // Check that the message is from a user and not a bot
-        if( message.type === "chat") {
-            // Answer to this user
-            rainbowSDK.im.sendMessageToJid("hello! How may I help you?", message.fromJid);
-            // Do something with the message sent
-            ...
+    rainbowSDK.events.on("rainbow_onmessagereceived", (message) => {
+
+        rainbowSDK.im.markMessageAsRead(message);
+        // Check if the message is not from you
+        if(!message.fromJid.includes(rainbowSDK.connectedUser.jid_im)) {
+            // Check that the message is from a user and not a bot
+            if( message.type === "chat") {
+                // Answer to this user
+
+                console.log(message);
+                rainbowSDK.im.sendMessageToJid("hello! How may I help you?", message.fromJid);
+                // Do something with the message sent
+                
+            }
         }
-    }
+    });
+
 });
 
-//if "sendReadReceipt": false
-rainbowSDK.events.on('rainbow_onmessagereceived', (message) => {
-    // do something with the message received
-    ...
-    // send manually a 'read' receipt to the sender
-    rainbowSDK.im.markMessageAsRead(message);
-});
