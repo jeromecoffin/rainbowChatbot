aa43548730fd87a5e74cf2d05488e7f393d66529
Camille Leou <camille.leou.tac.fong@efrei.net>
2020-05-29 20:58:05 +0200

    fichier js complet

--- bot.js
+++ bot.js
@@ -57,3 +57,57 @@ rainbowSDK.start().then(() => {
 
 });
 
+//Instantiate a dialogflow client
+const dialogflow = require('dialogflow');
+//project id
+const projectID = 'small-talks-ysnuou';
+
+async function runSample(projectID, query, message){
+    const sessionID = '123458';
+    const languageCode = 'en-US';
+    const credentials_fil_path ='C:\\Users\\camil\\Downloads\\small-talks-ysnuou-7bed293dbf2a.json';
+    const sessionClient = new dialogflow.SessionsClient({
+        projectID,
+        keyFilename: credentials_fil_path,
+    })
+    console.log("-----------------------------------request agent-----------------------------");
+    console.log("Message à envoyer à agnetl DialogFlow");
+    console.log(query);
+    console.log("projectID");
+    console.log(projectID);
+    console.log("sessionID");
+    console.log(sessionID);
+    console.log("credential_file_path");
+    console.log(credentials_fil_path);
+    console.log("sessionClient");
+    console.log(sessionClient);
+
+    const sessionPath = sessionClient.sessionPath(projectID, sessionID);
+
+    const request = {
+        session: sessionPath,
+
+        queryInput: {
+            text: {
+                text: query,
+                languageCode: 'en-US',
+            },
+        },
+    };
+
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent by agent dialogflow and answer');
+    console.log(responses);
+    const result = responses[0].queryResult;
+    console.log(`query: ${result.queryText}`);
+    console.log(`response: ${result.fulfillmentText}`);
+    if(result.intent) {
+        console.log(`Intent: ${result.intent.displayName}`);
+    }
+    else {
+        console.log(`No intent matched`);
+    }
+
+}
+
+runSample(projectID,"Camille", "");
